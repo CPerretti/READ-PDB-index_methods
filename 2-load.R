7b7186b4e2e176fa14658f6023ba3aa3bb1a19b1
cmlegault <chris.legault@noaa.gov>
2017-05-30 11:12:54 -0400

    change read.xlxs to read.csv

--- 2-load.R
+++ 2-load.R
@@ -3,5 +3,7 @@ source("1-func.R")
 
 data <- read.csv("data/GBYT_survey_data.csv")
 
-df_rfssb <- xlsx::read.xlsx("data/GBYTtimeseries.xlsx", 
-                            sheetName = "Sheet1")
+#df_rfssb <- xlsx::read.xlsx("data/GBYTtimeseries.xlsx", 
+#                            sheetName = "Sheet1")
+
+df_rfssb <- read.csv("data/GBYTtimeseries.csv")
